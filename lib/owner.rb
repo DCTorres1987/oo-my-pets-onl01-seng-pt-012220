3272f6587887aa7f5d924c2d47a6c8e2c2c3db52
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-02-21 00:46:12 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,36 @@
+require 'pry'
+
 class Owner
   # code goes here
+  attr_reader :name, :species, :cats 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @cats = cats
+    @species = "human"
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all 
+  end
+  
+  def say_species
+    "I am a human."
+  end
+  
+  def self.count 
+    self.all.count
+  end
+  
+  def self.reset_all
+    self.all.clear
+  end
+  
+  def cats
+    Cat.all.select {|cat| 
+  end
+ 
 end
